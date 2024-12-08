46b6b2e24660a6dedf33dbb5ac3f9d23cf92438c
Keerthik Muruganandam <70237309+krthkmgndm@users.noreply.github.com>
2024-12-08 15:37:53 -0600

    Changed the message for no posts.

--- fetchAllPosts.js
+++ fetchAllPosts.js
@@ -4,7 +4,7 @@ async function fetchBlogPosts() {
     const posts = await response.json();
 
     const blogContainer = document.getElementById("blog-container");
-    blogContainer.innerHTML = "Nothing's here :( Stay tuned!";
+    blogContainer.innerHTML = "No posts yet, but stay tuned!";
 
     posts
       .filter((post) => post.active !== 0)
